39b1286db1f3ed2f3426b11ee85dbc305c7711cc
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-12-11 13:13:14 +0100

    code added

--- app.js
+++ app.js
@@ -2,43 +2,56 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 let size = 20;
-let color = 'black'
-let isPressed = false
+let color = 'black';
+let isPressed = false;
 let x
 let y
 
 canvas.addEventListener('mousedown', (e) => {
-    isPressed = true
+	isPressed = true;
 
-    x = e.offsetX
-    y = e.offsetY
+	x = e.offsetX;
+	y = e.offsetY;
 
-    console.log(isPressed, x, y)
-})
+	// console.log(isPressed, x, y)
+});
 
-canvas.addEventListener('mousedup', (e) => {
-    isPressed = false
+canvas.addEventListener('mouseup', (e) => {
+	isPressed = false;
 
-    x = undefined
-    y = undefined
+	x = undefined;
+	y = undefined;
 
-    console.log(isPressed, x, y)
+	// console.log(isPressed, x, y)
+});
+
+canvas.addEventListener('mousemove', (e) => {
+    if(isPressed) {
+        const x2 = e.offsetX
+        const y2 = e.offsetY
+
+        drawCircle(x2, y2)
+        drawLine(x, y, x2, y2)
+
+        x = x2
+        y = y2
+    }
 })
 
 function drawCircle(x, y) {
-    ctx.beginPath();
-    ctx.arc(x, y, size, 0, Math.PI * 2)
-    ctx.fillStyle = color
-    ctx.fill()
+	ctx.beginPath();
+	ctx.arc(x, y, size, 0, Math.PI * 2);
+	ctx.fillStyle = color;
+	ctx.fill();
 }
 
 function drawLine(x1, y1, x2, y2) {
-    ctx.beginPath()
-    ctx.moveTo(x1, y1)
-    ctx.lineTo(x2, y2)
-    ctx.strokeStyle = color
-    ctx.lineWidth = size
-    ctx.stroke()
+	ctx.beginPath();
+	ctx.moveTo(x1, y1);
+	ctx.lineTo(x2, y2);
+	ctx.strokeStyle = color;
+	ctx.lineWidth = size;
+	ctx.stroke();
 }
 
 // drawCircle(100, 200)
